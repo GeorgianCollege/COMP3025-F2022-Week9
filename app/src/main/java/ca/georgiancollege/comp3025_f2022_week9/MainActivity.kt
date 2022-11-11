68eb735ef669ae45b12a95ac19c48ce7f3aa995e
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-11-11 11:20:58 -0500

    updated ReyclerView with new Data

--- app/src/main/java/ca/georgiancollege/comp3025_f2022_week9/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/comp3025_f2022_week9/MainActivity.kt
@@ -2,9 +2,11 @@ package ca.georgiancollege.comp3025_f2022_week9
 
 import android.annotation.SuppressLint
 import android.app.AlertDialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
+import android.util.Log
 import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,22 +14,23 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var addTVShowFAB: FloatingActionButton
+    var FavouriteTVShows = mutableListOf<TVShow>(
+        TVShow("House of the Dragon", "HBO"),
+        TVShow("Lord of the Rings", "Prime Video"),
+        TVShow("Andor", "Disney"),
+        TVShow("Severance", "AppleTv"),
+        TVShow("Star Trek: Strange New Worlds", "Paramount+"))
 
-    @SuppressLint("MissingInflatedId")
+    lateinit var addTVShowFAB: FloatingActionButton
+    lateinit var firstAdapter: FirstAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val FavouriteTVShows = arrayOf<TVShow>(
-            TVShow("House of the Dragon", "HBO"),
-            TVShow("Lord of the Rings", "Prime Video"),
-            TVShow("Andor", "Disney"),
-            TVShow("Severance", "AppleTv"),
-            TVShow("Star Trek: Strange New Worlds", "Paramount+"))
-
-        val firstAdapter: FirstAdapter = FirstAdapter(FavouriteTVShows)
+        firstAdapter = FirstAdapter(FavouriteTVShows)
         val recyclerView: RecyclerView = findViewById(R.id.First_Recycler_View)
+
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = firstAdapter
 
@@ -38,28 +41,23 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun showCreateTVShowDialog()
-    {
+    private fun showCreateTVShowDialog() {
         val dialogTitle = getString(R.string.dialog_title)
         val positiveButtonTitle = getString(R.string.add_tv_show)
-        val negativeButtonTitle = getString(R.string.cancel)
-
         val builder = AlertDialog.Builder(this)
-
-        val tvShowTitleEditText = findViewById<EditText>(R.id.TV_Show_Title_EditText)
-        val studioNameEditText = findViewById<EditText>(R.id.Studio_Name_EditText)
+        val view = layoutInflater.inflate(R.layout.add_new_tv_show_item, null)
 
         builder.setTitle(dialogTitle)
-        builder.setView(R.layout.add_new_tv_show_item)
+        builder.setView(view)
 
         builder.setPositiveButton(positiveButtonTitle) { dialog, _ ->
             dialog.dismiss()
+            val tvShowTitleEditText = view.findViewById<EditText>(R.id.TV_Show_Title_EditText)
+            val studioTitleEditText = view.findViewById<EditText>(R.id.Studio_Name_EditText)
+            val newTVShow = TVShow(tvShowTitleEditText.text.toString(), studioTitleEditText.text.toString())
+            FavouriteTVShows.add(newTVShow)
+            firstAdapter.notifyItemInserted(FavouriteTVShows.size)
         }
-
-        builder.setNegativeButton(negativeButtonTitle) { dialog, _ ->
-            dialog.dismiss()
-        }
-
         builder.create().show()
     }
 
